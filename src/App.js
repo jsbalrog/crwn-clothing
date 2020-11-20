0bc7ff46bee5dcda9574517e1bad2ed4365c7da4
Ted Jenkins <ted.jenkins@familysearch.org>
2020-11-20 14:08:15 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from 'react'
+import './App.css'
+import HomePage from './homepage.component'
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'>
+      <HomePage />
+    </div>
   }
 }
 
-export default App;
+export default App
